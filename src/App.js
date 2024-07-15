7261e83100093c43f4ab5d5b1f13f72b411eaf2f
Sbrjt <champa.281982@gmail.com>
2024-07-15 19:29:15 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -5,6 +5,26 @@ import Navbar from './Navbar'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
+	let installPrompt = null
+
+	window.addEventListener('beforeinstallprompt', (e) => {
+		installPrompt = e
+		console.log(installPrompt)
+	})
+
+	// show install pop-up if app not already installed
+	async function install() {
+		console.log(installPrompt)
+
+		if (installPrompt) {
+			await installPrompt.prompt()
+			const { outcome } = await installPrompt.userChoice
+			if (outcome === 'accepted') {
+				installPrompt = null
+			}
+		}
+	}
+
 	return (
 		<Router>
 			<div className='min-vh-100 d-flex flex-column justify-content-between'>
@@ -14,6 +34,9 @@ function App() {
 					<Route path='/member' element={<Member />} />
 					<Route path='*' element={<Home />} />
 				</Routes>
+				<button className='btn btn-outline-secondary btn-lg px-4' onClick={install}>
+					Install app
+				</button>
 				<Footer />
 			</div>
 		</Router>
--- src/Home.js
+++ src/Home.js
@@ -13,26 +13,6 @@ function Home() {
 		}
 	})
 
-	let installPrompt = null
-
-	window.addEventListener('beforeinstallprompt', (e) => {
-		e.preventDefault()
-		installPrompt = e
-	})
-
-	// show install pop-up if app not already installed
-	async function install() {
-		console.log(installPrompt)
-
-		if (installPrompt) {
-			await installPrompt.prompt()
-			const { outcome } = await installPrompt.userChoice
-			if (outcome === 'accepted') {
-				installPrompt = null
-			}
-		}
-	}
-
 	return (
 		<div className=' p-3 p-md-5 m-md-3 text-center bg-light'>
 			<div className='col-md-5 p-lg-5 mx-auto my-5'>
@@ -41,9 +21,6 @@ function Home() {
 				<div className='d-grid gap-2 d-sm-flex justify-content-sm-center'>
 					<button onClick={() => test().then((res) => console.log(res))} className='btn btn-primary btn-lg px-4 gap-3'>
 						Test backend
-					</button>
-					<button className='btn btn-outline-secondary btn-lg px-4' onClick={install}>
-						Install app
 					</button>
 				</div>
 			</div>
