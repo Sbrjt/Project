06d1823e0487011a0690ef62fc8de4a1611c7c50
Sbrjt <champa.281982@gmail.com>
2024-07-15 12:54:54 +0530

    ...

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,17 @@ import { auth, onAuthStateChanged } from './fb'
 import { test } from './fb'
 
 function Home() {
-	const [user, setUser] = useState('')
-	let installPrompt = null
+	const [user, setUser] = useState('User')
 
 	onAuthStateChanged(auth, (usr) => {
 		if (usr) {
 			setUser(usr.displayName)
+		} else {
+			setUser('')
 		}
 	})
+
+	let installPrompt = null
 
 	window.addEventListener('beforeinstallprompt', (e) => {
 		e.preventDefault()
@@ -33,8 +36,8 @@ function Home() {
 	return (
 		<div className=' p-3 p-md-5 m-md-3 text-center bg-light'>
 			<div className='col-md-5 p-lg-5 mx-auto my-5'>
-				<h1 className='display-4 fw-bold'>{user ? `Hi ${user}!` : `User`}</h1>
-				<p className='lead fw-normal'>{user ? `` : `Plz login :)`}</p>
+				<h1 className='display-4 fw-bold'>{user ? `Hi ${user}!` : `Hi User`}</h1>
+				<p className='lead fw-normal'>{user ? `Welcome 🎉` : `Plz login :)`}</p>
 				<div className='d-grid gap-2 d-sm-flex justify-content-sm-center'>
 					<button onClick={() => test().then((res) => console.log(res))} className='btn btn-primary btn-lg px-4 gap-3'>
 						Test backend
